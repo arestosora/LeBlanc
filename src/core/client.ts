ffafa7d03b5d4af6f8059fa61969ae82b84c4e71
Ares <89419093+arestosora@users.noreply.github.com>
2024-09-11 00:41:07 -0500

    feat: setup commit

---
+++ src/core/client.ts
@@ -0,0 +1,37 @@
+import { LogLevel, SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits, Partials } from "discord.js";
+
+export class Client extends SapphireClient {
+    constructor() {
+        super({
+            defaultPrefix: '!',
+            regexPrefix: /^(hey +)?bot[,! ]/i,
+            caseInsensitiveCommands: true,
+            logger: {
+                level: LogLevel.Debug
+            },
+            shards: 'auto',
+            intents: [
+                GatewayIntentBits.DirectMessageReactions,
+                GatewayIntentBits.DirectMessages,
+                GatewayIntentBits.GuildModeration,
+                GatewayIntentBits.GuildEmojisAndStickers,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildMessageReactions,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildVoiceStates,
+                GatewayIntentBits.MessageContent
+            ],
+            partials: [Partials.Channel],
+            loadMessageCommandListeners: true
+        })
+    }
+
+    public override async login(token?: string): Promise<string> {
+        return super.login(token)
+    }
+}
+
+const client = new Client();
+export default client;
